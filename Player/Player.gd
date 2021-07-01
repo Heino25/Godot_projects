da4318752215166701779c54ae4ab7a36256b156
Heino <heinoduplessis@gmail.com>
2021-07-01 08:05:43 +0200

    player animation

--- Player/Player.gd
+++ Player/Player.gd
@@ -14,3 +14,16 @@ func _process(delta):
 	if Input.is_action_pressed("ui_down"):
 		velocity.y += 1
 	move_and_slide(velocity.normalized() * speed)
+	player_animation(velocity)
+func player_animation(velocity):
+	if velocity.y > 0:
+		$"AnimatedSprite".play('walk_down')
+	elif velocity.y < 0:
+		$"AnimatedSprite".play('walk_up')
+		
+	elif velocity.x != 0:
+		$"AnimatedSprite".play('walk_side')
+		$"AnimatedSprite".flip_h = velocity.x > 0
+	else:
+		$"AnimatedSprite".stop()
+		$"AnimatedSprite".frame = 0
